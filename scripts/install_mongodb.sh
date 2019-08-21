fe7679cc6bf84bbe93f7ea700c7559d9839ffe53
gary leong <gear@thytruth.com>
2019-08-21 12:49:28 +0800

    update

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -4,7 +4,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 apt-get update && \
 apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make rng-tools openssl
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && \
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 apt-get update && \
