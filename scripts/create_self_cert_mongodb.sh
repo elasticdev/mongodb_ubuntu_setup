127e15385c82a7776f73acd7e90655123bcb6b13
gary leong <gear@thytruth.com>
2019-08-21 12:40:32 +0800

    first commit

--- scripts/create_self_cert_mongodb.sh
+++ scripts/create_self_cert_mongodb.sh
@@ -4,3 +4,6 @@ mkdir -p /etc/ssl
 
 openssl req -newkey rsa:2048 -new -x509 -subj "/C=US/ST=California/L=SanFrancisco/O=Dis/CN=www.selfsigned.com" -days 1024 -nodes -out mongodb.crt -keyout mongodb.key && \
 bash -c 'cat mongodb.crt mongodb.key > /etc/ssl/mongodb.pem'
+
+cp -rp mongod.conf /etc/mongodb.conf
+service mongod restart
--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
+apt-get update && \
+apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make rng-tools openssl
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && \
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 apt-get update && \
-
-apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make \
-libpcre3 libpcre3-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-thread-dev \
-libpcap-dev libreadline-dev libssl-dev rng-tools libcurl3 openssl
-
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-
-apt-get update
-
 apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+service mongod start
