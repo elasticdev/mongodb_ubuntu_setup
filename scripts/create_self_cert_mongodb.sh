334c05dd24ec4b8592342059783b580cc7a74174
gary leong <gear@thytruth.com>
2019-08-21 12:50:39 +0800

    update

--- scripts/create_self_cert_mongodb.sh
+++ scripts/create_self_cert_mongodb.sh
@@ -5,5 +5,7 @@ mkdir -p /etc/ssl
 openssl req -newkey rsa:2048 -new -x509 -subj "/C=US/ST=California/L=SanFrancisco/O=Dis/CN=www.selfsigned.com" -days 1024 -nodes -out mongodb.crt -keyout mongodb.key && \
 bash -c 'cat mongodb.crt mongodb.key > /etc/ssl/mongodb.pem'
 
+pwd
+
 cp -rp mongod.conf /etc/mongodb.conf
 service mongod restart
