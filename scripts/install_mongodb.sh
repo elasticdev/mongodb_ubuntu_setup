ea81766637ea38dfe00cb3d3af24dadd15b8a7b2
gary leong <gear@thytruth.com>
2019-08-21 15:50:32 +0800

    update

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -4,10 +4,10 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 apt-get update && \
 apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make rng-tools openssl
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
-apt-get update && \
+apt-get update
 apt-get install -y mongodb-org
 
 echo "mongodb-org hold" | sudo dpkg --set-selections
@@ -17,4 +17,3 @@ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
 service mongod start
-#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
