ecc8dae4a9ba15e71a776c9dc0a32270c8ec29a9
gary leong <gear@thytruth.com>
2019-08-21 17:14:54 +0800

    update

--- scripts/configure_mount_volume.sh
+++ scripts/configure_mount_volume.sh
@@ -5,3 +5,4 @@ rm -rf /var/lib/mongodb/*
 mkfs.xfs /dev/sda && \
 echo "/dev/sda  /var/lib/mongodb xfs defaults,nofail,discard 0 0" >> /etc/fstab && \
 mount /var/lib/mongodb
+chown -R mongodb:mongodb
