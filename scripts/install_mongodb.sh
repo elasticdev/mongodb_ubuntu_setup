80581048bd26932e7cc2ad0e300c6fc7ec30bc42
Gary <gary@elasticdev.io>
2020-04-20 15:05:23 -0700

    update

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -2,8 +2,9 @@
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 apt-get update && \
-apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make rng-tools openssl
+apt-get install -y pwgen wget curl git-core build-essential scons devscripts lintian dh-make rng-tools openssl vim git curl wget gnupg -y
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
